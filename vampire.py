cb5345a7e18bfbd9e080e89214f50bd0d2706c56
Josephinetech <120250130+Josephinetech@users.noreply.github.com>
2023-10-20 20:35:58 -0700

    Add files via upload

---
+++ vampire.py
@@ -0,0 +1,13 @@
+while True:
+    print(' Who are you? ')
+    name = input()
+    if name != 'dee':
+        continue
+    print('Hello dee, what is the password? (it is a fish)')
+    password = input()
+    if password == 'swordfish':
+        break
+    print('access granted')
+    
+
+   
